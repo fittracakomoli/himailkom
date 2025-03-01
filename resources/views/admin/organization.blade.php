8a18568625d955fc9bca03a5049ab38d1042d9a5
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-03-01 23:20:14 +0700

    l

--- resources/views/admin/organization.blade.php
+++ resources/views/admin/organization.blade.php
@@ -9,53 +9,53 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900"> 
-                    <form action="{{ route('organization.update') }}" method="POST">
+                    <form action="#" method="POST">
                         @csrf
                         @method('PATCH')
                         <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
                             <div class="sm:col-span-2">
                                 <x-input-label for="nama" :value="__('Nama Organisasi')" />
-                                <x-text-input id="nama" name="nama" type="text" class="mt-1 block w-full" :value="old('nama', $organization->nama)" required autocomplete="name" />
+                                <x-text-input id="nama" name="nama" type="text" class="mt-1 block w-full" :value="old('nama', )" required autocomplete="name" />
                                 <x-input-error class="mt-2" :messages="$errors->get('nama')" />
                             </div>
                             <div class="w-full">
                                 <x-input-label for="kabinet" :value="__('Nama Kabinet')" />
-                                <x-text-input id="kabinet" name="kabinet" type="text" class="mt-1 block w-full" :value="old('kabinet', $organization->kabinet)" required autocomplete="off" />
+                                <x-text-input id="kabinet" name="kabinet" type="text" class="mt-1 block w-full" :value="old('kabinet',)" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('kabinet')" />
                             </div>
                             <div class="w-full">
                                 <x-input-label for="periode" :value="__('Periode')" />
-                                <x-text-input id="periode" name="periode" type="text" class="mt-1 block w-full" :value="old('periode', $organization->periode)" required autocomplete="off" />
+                                <x-text-input id="periode" name="periode" type="text" class="mt-1 block w-full" :value="old('periode',)" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('periode')" />
                             </div>
                             <div class="sm:col-span-2">
                                 <x-input-label for="slogan" :value="__('Slogan')" />
-                                <x-text-input id="slogan" name="slogan" type="text" class="mt-1 block w-full" :value="old('slogan', $organization->slogan)" required autocomplete="off" />
+                                <x-text-input id="slogan" name="slogan" type="text" class="mt-1 block w-full" :value="old('slogan',)" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('slogan')" />
                             </div>
                             <div class="sm:col-span-2">
                                 <x-input-label for="deskripsi" :value="__('Deskripsi')" />
-                                <x-text-input id="deskripsi" name="deskripsi" type="text" class="mt-1 block w-full" :value="old('deskripsi', $organization->deskripsi)" required autocomplete="off" />
+                                <x-text-input id="deskripsi" name="deskripsi" type="text" class="mt-1 block w-full" :value="old('deskripsi',)" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('deskripsi')" />
                             </div>
                             <div class="sm:col-span-2">
                                 <x-input-label for="sambutan" :value="__('Sambutan')" />
-                                <x-text-input id="sambutan" name="sambutan" type="text" class="mt-1 block w-full" :value="old('sambutan', $organization->sambutan)" required autocomplete="off" />
+                                <x-text-input id="sambutan" name="sambutan" type="text" class="mt-1 block w-full" :value="old('sambutan',)" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('sambutan')" />
                             </div>
                             <div class="sm:col-span-2">
                                 <x-input-label for="pendamping" :value="__('Pendamping Himpunan')" />
-                                <x-text-input id="pendamping" name="pendamping" type="text" class="mt-1 block w-full" :value="old('pendamping', $organization->pendamping)" required autocomplete="off" />
+                                <x-text-input id="pendamping" name="pendamping" type="text" class="mt-1 block w-full" :value="old('pendamping',)" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('pendamping')" />
                             </div>
                             <div class="sm:col-span-2">
                                 <x-input-label for="visi" :value="__('Visi Himpunan')" />
-                                <x-text-input id="visi" name="visi" type="text" class="mt-1 block w-full" :value="old('visi', $organization->visi)" required autocomplete="off" />
+                                <x-text-input id="visi" name="visi" type="text" class="mt-1 block w-full" :value="old('visi', )" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('visi')" />
                             </div>
                             <div class="sm:col-span-2">
                                 <x-input-label for="misi" :value="__('Misi Himpunan')" />
-                                <x-text-input id="misi" name="misi" type="text" class="mt-1 block w-full" :value="old('misi', $organization->misi)" required autocomplete="off" />
+                                <x-text-input id="misi" name="misi" type="text" class="mt-1 block w-full" :value="old('misi', )" required autocomplete="off" />
                                 <x-input-error class="mt-2" :messages="$errors->get('misi')" />
                             </div>
                         </div>
